8fe634fcdb5c47a4dbc0aaa5c615bf41ff38052e
ashandimantha2000 <ashandimanthalk@gmail.com>
2023-06-17 15:38:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Nav'
+
+// import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <img src="../src/assets/Airbnb_Logo.png" className="nav-logo" />
+            
+        </nav>
+    )
+}
