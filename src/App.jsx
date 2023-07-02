bf246ba80ff02c9358c7ae63eeca1b06fd8a8e72
ashandimantha2000 <ashandimanthalk@gmail.com>
2023-07-02 14:18:58 +0530

    added:Contct cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Nav'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import Test from './components/temp'
 
 // import './App.css'
 
@@ -11,6 +12,7 @@ export default function App() {
       <Navbar />
       <Hero />
       <Card />
+      <Test />
     </div>
   )
 }
